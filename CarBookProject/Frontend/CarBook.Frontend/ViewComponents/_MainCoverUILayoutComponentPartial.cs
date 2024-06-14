1ff5413c30ffbc031b94c87d257847742905ae58
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2024-06-14 21:24:03 +0300

    StartingViewComponents

---
+++ CarBookProject/Frontend/CarBook.Frontend/ViewComponents/_MainCoverUILayoutComponentPartial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.Frontend.ViewComponents
+{
+	public class _MainCoverUILayoutComponentPartial : ViewComponent
+	{
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
