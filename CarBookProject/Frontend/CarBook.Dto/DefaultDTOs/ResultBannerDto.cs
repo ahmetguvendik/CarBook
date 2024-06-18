c65015489ef0377b5810128c9a9dbd60547d78da
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2024-06-18 12:17:57 +0300

    StartingHomePage

---
+++ CarBookProject/Frontend/CarBook.Dto/DefaultDTOs/ResultBannerDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CarBook.Dto.DefaultDTOs
+{
+	public class ResultBannerDto
+	{
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string VideoDescription { get; set; }
+        public string VideoURL { get; set; }
+    }
+}
+
